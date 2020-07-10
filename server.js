8c42ce224ea09b16b3c824438362466a63d84cc4
Kingsley <perfectman090@gmail.com>
2020-07-10 22:43:49 +0100

    modified package.json

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 5000;
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(expree.static('client/build'));
+    app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
