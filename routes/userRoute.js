337ea6e0134f8c9e62ad3ee85b19e82fb0d2306e
king <perfectman090@gmail.com>
2021-08-27 21:07:06 +0100

    added verifytoken middleware

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import UserController from '../controllers/userController';
+import AuthMiddleware from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 const usercontroller = new UserController();
+const authmiddleware = new AuthMiddleware();
 
 router.get('/all', usercontroller.homePage);
-router.get('/user', usercontroller.userPage);
+router.get('/user', authmiddleware.verifyToken, usercontroller.userPage);
 router.post('/signup', usercontroller.register);
 router.post('/signin', usercontroller.login);
 
