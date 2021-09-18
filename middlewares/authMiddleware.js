78ca674d73f1148023b350f9e5770972b8c7b260
King <kingskaka576@gmail.com>
2021-09-18 11:33:31 +0100

    added auth middleware

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,37 +1,26 @@
 const jwt = require("jsonwebtoken");
 
-class AuthMiddleware {
-
-    /**
-     * @method verifyToken
-     * @description verify token
-     * @param {object} req - The Request Object
-     * @param {object} res - The Response Object
-     * @returns {object} JSON API Response
-     */
-    async verifyToken(req, res) {
-        let token = req.headers['x-access-token'];
-
-        // Check for token
-        if (!token) {
-            return res.status(401).json({ message: 'No token, authorization denied!' })
-        }
-
-        try {
-
-            // Verify Token
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            //console.log(decoded);
-            if (!decoded) throw Error('unable to decode token');
-
-            // Add user from payload
-            req.user = decoded.id;
-           // console.log(decoded.id);
-        
-        } catch (e) {
-            res.status(400).json({ message: e.message })
-        }
+const verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+  
+    if (!token) {
+      return res.status(403).send({ message: "No token provided!" });
     }
-}    
+  
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+     
+        //Add user from payload
+        req.user = decoded.id;
+        next();
+    } catch (e) {
+        res.status(400).json({ message: e.message });
+    }
+  };
 
-export default AuthMiddleware;
+  const auth = {
+    verifyToken
+  };
+
+  module.exports = auth;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import UserController from '../controllers/userController';
-import AuthMiddleware from '../middlewares/authMiddleware';
+import auth from '../middlewares/authMiddleware';
 
 const router = express.Router();
 
 const usercontroller = new UserController();
-const authmiddleware = new AuthMiddleware();
 
 router.get('/all', usercontroller.homePage);
-router.get('/user', authmiddleware.verifyToken, usercontroller.userPage);
+router.get('/user', auth.verifyToken, usercontroller.userPage);
 router.post('/signup', usercontroller.register);
 router.post('/signin', usercontroller.login);
 
